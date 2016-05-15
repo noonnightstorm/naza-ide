32e5ddb006ceff761a2281a74a985b4fb466cbc3
无惟 <guangbin.lianggb@alibaba-inc.com>
2016-05-15 18:02:14 +0800

    finish generator

--- db/init.js
+++ db/init.js
@@ -6,78 +6,92 @@ function init(pool) {
   //先清除数据
   // deleteData(pool);
   //生成用户表格
-  createUserTable(pool);
+  var userPromise = createUserTable(pool);
   //生成文件表格
-  createFileTable(pool);
+  var filePromise = createFileTable(pool);
   //生成用户－文件的表格
-  createLinkTable(pool);
+  var linkPromise = createLinkTable(pool);
+
 }
 
 function deleteData(pool) {
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.DeleteData[0], function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  var userPromise = new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.DeleteData[0], function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+        resolve();
+      });
     });
   });
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.DeleteData[1], function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  var filePromise = new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.DeleteData[1], function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+      });
     });
   });
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.DeleteData[2], function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  var linkPromise = new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.DeleteData[2], function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+      });
     });
   });
+
+  return Promise.all([userPromise, filePromise, linkPromise]);
 }
 
 function createUserTable(pool) {
-
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.CreateUser, function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  return new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.CreateUser, function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        resolve();
+        connection.release();
+      });
     });
   });
-
 }
 
 function createFileTable(pool) {
-
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.CreateFileList, function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  return new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.CreateFileList, function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        resolve();
+        connection.release();
+      });
     });
   });
-
 }
 
 function createLinkTable(pool) {
-
-  pool.getConnection(function(err, connection) {
-    connection.query(SQL.CreateUserFileLink, function(err, rows) {
-      if (err) {
-        console.log(err);
-      }
-      connection.release();
+  return new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.CreateUserFileLink, function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        resolve();
+        connection.release();
+      });
     });
   });
-
 }
+
 
 
 module.exports = init;
