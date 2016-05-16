507e6c1f37a6e8b7c1e968f8ad929d23583d2e76
无惟 <guangbin.lianggb@alibaba-inc.com>
2016-05-16 14:10:40 +0800

    draw back

--- db/init.js
+++ db/init.js
@@ -4,28 +4,28 @@ var SQL = require("./sql");
 
 function init(pool) {
   //先清除数据
-  deleteData(pool);
+  // deleteData(pool);
   //生成用户表格
-  // var userPromise = createUserTable(pool);
-  // //生成文件表格
-  // var filePromise = createFileTable(pool);
-  // //生成用户－文件的表格
-  // var linkPromise = createLinkTable(pool);
+  var userPromise = createUserTable(pool);
+  //生成文件表格
+  var filePromise = createFileTable(pool);
+  //生成用户－文件的表格
+  var linkPromise = createLinkTable(pool);
 
 }
 
 function deleteData(pool) {
-  // var userPromise = new Promise(function(resolve, reject) {
-  //   pool.getConnection(function(err, connection) {
-  //     connection.query(SQL.DeleteData[0], function(err, rows) {
-  //       if (err) {
-  //         console.log(err);
-  //       }
-  //       connection.release();
-  //       resolve();
-  //     });
-  //   });
-  // });
+  var userPromise = new Promise(function(resolve, reject) {
+    pool.getConnection(function(err, connection) {
+      connection.query(SQL.DeleteData[0], function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+        resolve();
+      });
+    });
+  });
   var filePromise = new Promise(function(resolve, reject) {
     pool.getConnection(function(err, connection) {
       connection.query(SQL.DeleteData[1], function(err, rows) {
